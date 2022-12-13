27ae6b9119873c0954a00723a32752ca6f8390d7
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-12-12 20:29:32 -0600

    Clase 54 StatefulWidget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,28 +1,45 @@
 
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({ Key? key }) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+} 
+
+class _HomeScreenState extends State<HomeScreen> {
+  int counter = 15;
+  
   @override
   Widget build(BuildContext context) {
     const fontSize30 =  TextStyle(fontSize: 30); //Tamaño de la letra
+    
     return  Scaffold(
       appBar: AppBar(
-        title: const Text("HomeScreen"),
+        title: const Text("CounterScreen"),
         elevation: 0,
       ),
       body:  Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center, //alinea hacia abajo
           //crossAxisAlignment: CrossAxisAlignment.end,                         //Alinea los hijos respecto a el padre.
-          children:  const <Widget>[
-              Text("Número de Clicks", style: fontSize30 ), //fontSize30 utiliza el estilo
-              Text("10", style: fontSize30),
+          children:   <Widget>[
+              const Text("Número de Clicks", style: fontSize30 ), //fontSize30 utiliza el estilo
+              Text( "$counter", style: fontSize30 ),
           ],
         ),
-      )
+      ),
+      //floatingActionButtonLocation   mueve la posición del boton. 
+      //floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterFloat,
+      floatingActionButton:  FloatingActionButton(
+        child: const Icon(Icons.car_repair),
+        onPressed: () {  
+          counter++;
+          setState(() {});
+        },
+      
+      ),
     );  
   }
-
 }
