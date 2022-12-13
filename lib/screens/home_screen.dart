4a39c174f159a2b2714b0f65e7be59a7694c2879
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-12-12 18:50:10 -0600

    Avance a clase 51 Estilos de textos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,9 +6,23 @@ class HomeScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("HomeScreen")
-    );
+    const fontSize30 =  TextStyle(fontSize: 30); //Tamaño de la letra
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("HomeScreen"),
+        elevation: 0,
+      ),
+      body:  Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, //alinea hacia abajo
+          //crossAxisAlignment: CrossAxisAlignment.end,                         //Alinea los hijos respecto a el padre.
+          children:  const <Widget>[
+              Text("Número de Clicks", style: fontSize30 ), //fontSize30 utiliza el estilo
+              Text("10", style: fontSize30),
+          ],
+        ),
+      )
+    );  
   }
 
 }
